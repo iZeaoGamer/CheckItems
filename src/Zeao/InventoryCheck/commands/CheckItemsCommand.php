eca721e0732933bd471e22e04e9fef7e47a29648
VMPE Team <jameshayward14@icloud.com>
2019-08-25 20:39:06 +0100

    Implement Player:: class
    What's wrong with me? Lmao.

--- src/Zeao/InventoryCheck/commands/CheckItemsCommand.php
+++ src/Zeao/InventoryCheck/commands/CheckItemsCommand.php
@@ -6,6 +6,7 @@ use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use Zeao\InventoryCheck\InventoryChecker;
 use pocketmine\utils\TextFormat;
+use pocketmine\Player;
 
 class CheckItemsCommand extends Command{
     public function __construct(InventoryChecker $plugin){
